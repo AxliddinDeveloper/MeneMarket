254effb5893803e949b369038995edd2866f92a1
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-02-12 09:15:05 +0500

    INFRA: Initialize API Project

---
+++ MeneMarket/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace MeneMarket.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public IActionResult Running()
+        {
+            return Ok("MeneMarket is running...");
+        }
+    }
+}
