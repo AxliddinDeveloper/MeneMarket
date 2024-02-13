84779e6a74a919f1d324ca6b0caf763d21a4d960
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-02-13 15:53:42 +0500

    POC: User CRUD

---
+++ MeneMarket/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using MeneMarket.Models.Foundations.Users;
+using MeneMarket.Services.Foundations.Users;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace MeneMarket.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : RESTFulController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<User>> PostUser(User user) =>
+            await this.userService.AddUserAsync(user);
+
+        [HttpGet]
+        public ActionResult<IQueryable<User>> GelAllUsers()
+        {
+            IQueryable<User> allUserss =
+                this.userService.RetrieveAllUsers();
+
+            return Ok(allUserss);
+        }
+
+        [HttpGet("ById")]
+        public async ValueTask<ActionResult<User>> GetUserByIdAsync(Guid id) =>
+            await this.userService.RetrieveUserByIdAsync(id);
+
+        [HttpPut]
+        public async ValueTask<ActionResult<User>> PutHome(User user) =>
+            await this.userService.ModifyUserAsync(user);
+
+        [HttpDelete]
+        public async ValueTask<ActionResult<User>> DeleteUser(Guid id) =>
+            await this.userService.RemoveUserAsync(id);
+    }
+}
--- MeneMarket/Program.cs
+++ MeneMarket/Program.cs
@@ -1,4 +1,5 @@
 using MeneMarket.Brokers.Storages;
+using MeneMarket.Services.Foundations.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StorageBroker>();
 builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+builder.Services.AddTransient<IUserService, UserService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
