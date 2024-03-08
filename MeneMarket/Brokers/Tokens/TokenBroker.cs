0f49ba5721443ffb0fb1aa8f39a124b4fffec8f2
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-03-08 15:50:49 +0500

    refactore token

--- MeneMarket/Brokers/Tokens/TokenBroker.cs
+++ MeneMarket/Brokers/Tokens/TokenBroker.cs
@@ -24,7 +24,7 @@ namespace MeneMarket.Brokers.Tokens
                 new Claim(ClaimTypes.Role,user.Role.ToString()),
             };
 
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Jwt:Key").Value));
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6AD2EF4v89d4v9dse98df784DE-Ada98f4as894B2C-48sd4v98s7v98sd78dg8d49g");
 
             var signingCred = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
 
